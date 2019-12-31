0e35d33698a3df25e47718b52c43b2481523bb45
n0k5 <hey@chrish.design>
2019-12-31 11:19:15 +0000

    [~] Fix eslint error

--- src/components/container.js
+++ src/components/container.js
@@ -3,16 +3,14 @@ import styled from '@emotion/styled'
 import tw from 'tailwind.macro'
 
 const Container = ({ children }) => {
-  {
-    /**
-     * Recreating the Tailwind container as we don't
-     * have access to plugins with this setup.
-     *
-     * 1. Set some breakpoints from Tailwinds defaults
-     * 2. Loop through these to set our media queries
-     *
-     */
-  }
+  /**
+   * Recreating the Tailwind container as we don't
+   * have access to plugins with this setup.
+   *
+   * 1. Set some breakpoints from Tailwinds defaults
+   * 2. Loop through these to set our media queries
+   *
+   */
   const breakpoints = [640, 768, 1024, 1280]
   const PageContainer = styled.div`
     ${tw`
