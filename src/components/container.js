8080c6ec5215b57f8f62f2ddf4a3e686bc8505a2
n0k5 <hey@chrish.design>
2019-12-31 11:11:17 +0000

    [~] Move logic within component

--- src/components/container.js
+++ src/components/container.js
@@ -2,26 +2,29 @@ import React from 'react'
 import styled from '@emotion/styled'
 import tw from 'tailwind.macro'
 
-{
-  /**
-   * Recreating the Tailwind container as we don't
-   * have access to plugins with this setup.
-   *
-   * 1. Set some breakpoints from Tailwinds defaults
-   * 2. Loop through these to set our media queries
-   *
-   */
-}
-const breakpoints = [640, 768, 1024, 1280]
-const PageContainer = styled.div`
-  ${tw`
-    mx-auto px-4
-  `}
-  ${breakpoints.map(
-    minWidth => `@media(min-width: ${minWidth}px) { max-width: ${minWidth}px }`
-  )}
-`
+const Container = ({ children }) => {
+  {
+    /**
+     * Recreating the Tailwind container as we don't
+     * have access to plugins with this setup.
+     *
+     * 1. Set some breakpoints from Tailwinds defaults
+     * 2. Loop through these to set our media queries
+     *
+     */
+  }
+  const breakpoints = [640, 768, 1024, 1280]
+  const PageContainer = styled.div`
+    ${tw`
+      mx-auto px-4
+    `}
+    ${breakpoints.map(
+      minWidth =>
+        `@media(min-width: ${minWidth}px) { max-width: ${minWidth}px }`
+    )}
+  `
 
-const Container = ({ children }) => <PageContainer>{children}</PageContainer>
+  return <PageContainer>{children}</PageContainer>
+}
 
 export default Container
