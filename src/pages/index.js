805d2f68266179bde74248238a9768d40c4401fe
n0k5 <hey@chrish.design>
2019-12-31 11:51:37 +0000

    [+] Basic index layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import styled from '@emotion/styled'
-import tw from 'tailwind.macro'
 import { Helmet } from 'react-helmet'
+import Layout from '../components/layout'
+import Header from '../components/header'
 
 export default ({ data }) => (
   <Layout>
+    {/**
+     * Set some meta data
+     */}
     <Helmet>
       <meta charSet="utf-8" />
       <title>{data.site.siteMetadata.title}</title>
     </Helmet>
+
+    {/**
+     * The main page header
+     */}
+    <Header siteTitle={data.site.siteMetadata.title} />
   </Layout>
 )
 
