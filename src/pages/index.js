e6d55006ad3896887c3d93881c2706a5614a40ab
n0k5 <hey@chrish.design>
2020-01-04 12:19:33 +0000

    [+] Add a bit of detail about me to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,53 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
 import Layout from '../components/layout'
 import Header from '../components/header'
+import Container from '../components/container'
+import tw from 'tailwind.macro'
+import styled from '@emotion/styled'
 
-export default ({ data }) => (
-  <Layout>
-    {/**
-     * Set some meta data
-     */}
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>{data.site.siteMetadata.title}</title>
-    </Helmet>
+export default () => {
+  /**
+   * Function to pick a random colour from an array
+   */
+  const colour = [
+    'tomato',
+    'MediumSeaGreen',
+    'MediumPurple',
+    'LightSeaGreen',
+    'DodgerBlue',
+  ]
+  const rand = Math.floor(Math.random() * colour.length)
+  console.log(rand)
 
-    {/**
-     * The main page header
-     */}
-    <Header siteTitle={data.site.siteMetadata.title} />
-  </Layout>
-)
+  /**
+   * Styling for the description of what I do
+   */
+  const WhoAmI = styled.h1`
+    ${tw`
+    text-2xl
+    flex
+    flex-col
+    justify-center
+    sm:w-8/12
+    md:w-7/12
+    lg:w-6/12
+  `}
+    color: ${colour[rand]};
+    height: calc(100vh - 56px);
+    
+  `
 
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+  return (
+    <Layout>
+      <Header />
+      <section>
+        <Container>
+          <WhoAmI>
+            Designer / developer currently UI/UX Designer at Sky Betting &amp;
+            Gaming.
+          </WhoAmI>
+        </Container>
+      </section>
+    </Layout>
+  )
+}
