4340abf9f26feac87d69d9b7d580a93a5acb922c
n0k5 <hey@chrish.design>
2020-01-05 10:34:14 +0000

    [~] Base styling

--- src/components/typography/wavyBaseline.js
+++ src/components/typography/wavyBaseline.js
@@ -9,27 +9,30 @@ const TextBox = styled.svg`
 `
 
 const WavyBaseline = () => (
-  <div css={tw`bg-black`}>
-    <Helmet>
-      <link
-        href="https://fonts.googleapis.com/css?family=Gothic+A1:900"
-        rel="stylesheet"
-      />
-    </Helmet>
+  <div>
+    <pre css={tw`text-sm mb-2`}>wavyBaseline.js</pre>
+    <div css={tw`bg-black`}>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css?family=Gothic+A1:900"
+          rel="stylesheet"
+        />
+      </Helmet>
 
-    <TextBox viewBox="0 0 56 18">
-      <text
-        x="0"
-        y="15"
-        css={css`
-          fill: white;
-          text-transform: uppercase;
-          font-family: 'Gothic A1', serif;
-        `}
-      >
-        관박쥐
-      </text>
-    </TextBox>
+      <TextBox viewBox="0 0 56 18">
+        <text
+          x="0"
+          y="15"
+          css={css`
+            fill: white;
+            text-transform: uppercase;
+            font-family: 'Gothic A1', serif;
+          `}
+        >
+          관박쥐
+        </text>
+      </TextBox>
+    </div>
   </div>
 )
 
