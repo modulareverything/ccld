c7c84209f298d6cde4b4e5e0eeff20e9abc70a7b
n0k5 <hey@chrish.design>
2020-01-06 16:56:17 +0000

    [+] Interpolation fun

--- src/components/microanimation/toggleSpring.js
+++ src/components/microanimation/toggleSpring.js
@@ -4,21 +4,28 @@ import tw from 'tailwind.macro'
 
 const ToggleSpring = () => {
   const [isToggled, setToggle] = useState(false)
-  const fade = useSpring({
-    color: isToggled ? 'tomato' : 'green',
-    fontSize: isToggled ? '2rem' : '1rem',
+  const { color, x } = useSpring({
+    color: isToggled ? 'tomato' : 'white',
+    x: isToggled ? 50 : 0,
   })
 
   return (
     <div>
       <pre css={tw`text-sm mb-2`}>toggleSpring.js</pre>
       <div css={tw`bg-gray-800 p-4 text-gray-200`}>
-        <animated.p style={fade}>A 👏🏻 very 👏🏻 simple 👏🏻 toggle</animated.p>
+        <animated.p
+          style={{
+            transform: x.interpolate(x => `translate3d(${x}px, 0, 0)`),
+            color,
+          }}
+        >
+          A 👏🏻 simple 👏🏻 toggle
+        </animated.p>
         <button
           css={tw`mt-4 border py-1 px-4 border-gray-800 bg-gray-600 text-white text-sm uppercase`}
           onClick={() => setToggle(!isToggled)}
         >
-          {isToggled ? 'Nice 🕶' : 'Push and see'}
+          {isToggled ? 'Nice' : 'Push and see'}
         </button>
       </div>
     </div>
