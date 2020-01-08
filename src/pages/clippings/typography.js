e27c46a3888746f67593c257a40d0e3deac96365
n0k5 <hey@chrish.design>
2020-01-08 10:10:53 +0000

    [~] Export default component

--- src/pages/clippings/typography.js
+++ src/pages/clippings/typography.js
@@ -5,37 +5,33 @@ import Layout from '../../components/layout'
 import Container from '../../components/container'
 import WavyBaseline from '../../components/typography/wavyBaseline'
 
-class Typography extends React.Component {
-  render() {
-    return (
-      <Layout>
-        {/**
-         * The main page header
-         */}
-        <Header pageTitle="Clippings / Typography" />
+export default () => {
+  return (
+    <Layout>
+      {/**
+       * The main page header
+       */}
+      <Header pageTitle="Clippings / Typography" />
 
-        {/**
-         * The main content
-         */}
-        <section>
-          <Container>
-            <p
-              css={tw`
+      {/**
+       * The main content
+       */}
+      <section>
+        <Container>
+          <p
+            css={tw`
                 text-sm
                 mb-8
               `}
-            >
-              A few typography experiments.
-            </p>
+          >
+            A few typography experiments.
+          </p>
 
-            <hr css={tw`mb-8`} />
+          <hr css={tw`mb-8`} />
 
-            <WavyBaseline />
-          </Container>
-        </section>
-      </Layout>
-    )
-  }
+          <WavyBaseline />
+        </Container>
+      </section>
+    </Layout>
+  )
 }
-
-export default Typography
