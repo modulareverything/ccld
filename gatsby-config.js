a81ef6baf4bc240eb1ba8b716ec21c0b1c757d34
Chrish <chrish@chrishs-mbp.home>
2020-02-10 18:14:42 +0000

    [+] Add typography styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,11 @@ module.exports = {
       resolve: `gatsby-plugin-postcss`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `./src/utils/typography`,
+      },
+    },
   ],
 };
---
+++ src/utils/typography.js
@@ -0,0 +1,25 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  includeNormalize: false,
+  baseFontSize: '22px',
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    'Founders Grotesk',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  bodyFontFamily: [
+    'Founders Grotesk',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+});
+
+export default typography;
