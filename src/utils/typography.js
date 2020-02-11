a0ba8c5fc3d72e8236c5804e45c62aad7a768f0d
n0k5 <hey@chrish.design>
2020-02-11 11:54:04 +0000

    [~] Reduce base font size

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography';
 
 const typography = new Typography({
-  baseFontSize: '20px',
+  baseFontSize: '18px',
   baseLineHeight: 1.666,
   headerWeight: 500,
   headerFontFamily: [
