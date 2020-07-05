fa411c175c76df145f9afaba964f1ed7185bc798
Chrish Dunne <hey@chrish.design>
2020-07-05 19:21:53 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# chrish.design website
---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `chrish.design`,
+    description: `Front-end web developer amongst other things`,
+    author: `Chrish Dunne <hey@chrish.design>`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
